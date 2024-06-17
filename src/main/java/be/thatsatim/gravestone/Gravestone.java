6224b58975b550633bed750ca22f4037d8197ba4
Stualyttle Kirry <Stualyttle@users.noreply.github.com>
2024-06-17 21:00:21 +0200

    Add Vault (broken)

--- src/main/java/be/thatsatim/gravestone/Gravestone.java
+++ src/main/java/be/thatsatim/gravestone/Gravestone.java
@@ -3,28 +3,34 @@ package be.thatsatim.gravestone;
 import be.thatsatim.gravestone.commands.RetrieveGraveStoneCommand;
 import be.thatsatim.gravestone.database.GravestoneDatabase;
 import be.thatsatim.gravestone.listeners.*;
-import com.mojang.brigadier.arguments.StringArgumentType;
-import com.mojang.brigadier.builder.LiteralArgumentBuilder;
-import com.mojang.brigadier.builder.RequiredArgumentBuilder;
-import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
 import io.papermc.paper.plugin.lifecycle.event.LifecycleEventManager;
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.RegisteredServiceProvider;
 
 import java.sql.SQLException;
-import java.util.List;
 
 public final class Gravestone extends JavaPlugin {
-
     private GravestoneDatabase gravestoneDatabase;
+    private Economy economy;
 
     @Override
     public void onEnable() {
+        // Create the default config if it doesn't exist
         this.saveDefaultConfig();
+
+        // Setup Vault
+        if (!setupEconomy()) {
+            getLogger().severe("Vault not found or Economy not supported! Disabling plugin.");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        // Register the listeners
         new Death(this);
         new RightClick(this);
         new BreakBlock(this);
@@ -32,12 +38,14 @@ public final class Gravestone extends JavaPlugin {
         new InventoryClick(this);
         new InventoryDrag(this);
 
+        // Register the commands
         LifecycleEventManager<Plugin> manager = this.getLifecycleManager();
         manager.registerEventHandler(LifecycleEvents.COMMANDS, event -> {
             final Commands commands = event.registrar();
             this.registerCommands(commands);
         });
 
+        // Create the database connection
         try {
             if (!getDataFolder().exists()) {
                 getDataFolder().mkdirs();
@@ -55,13 +63,33 @@ public final class Gravestone extends JavaPlugin {
     @Override
     public void onDisable() {
         try {
-            gravestoneDatabase.closeConnection();
+            if (gravestoneDatabase != null) {
+                gravestoneDatabase.closeConnection();
+            }
         } catch (SQLException exception) {
             exception.printStackTrace();
         }
     }
 
-    public static void registerCommands(Commands commands) {
-        RetrieveGraveStoneCommand.register(commands);
+    public void registerCommands(Commands commands) {
+        RetrieveGraveStoneCommand.register(this, commands);
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+
+        economy = rsp.getProvider();
+        return economy != null;
+    }
+
+    public Economy getEconomy() {
+        return economy;
     }
 }
--- src/main/java/be/thatsatim/gravestone/commands/RetrieveGraveStoneCommand.java
+++ src/main/java/be/thatsatim/gravestone/commands/RetrieveGraveStoneCommand.java
@@ -11,6 +11,11 @@ import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -23,15 +28,22 @@ import java.util.List;
 import java.util.UUID;
 
 public class RetrieveGraveStoneCommand implements Command<CommandSourceStack> {
+    // Import Economy from Vault
+    private static Economy economy = null;
 
-    public static void register(Commands commands) {
+    public static void register(Gravestone plugin, Commands commands) {
+        // Get the economy from Vault
+        economy = plugin.getEconomy();
+
         LiteralArgumentBuilder<CommandSourceStack> commandBuilder =
             Commands.literal("retrieve-gravestone")
                 .then(RequiredArgumentBuilder.<CommandSourceStack, String>argument("world", StringArgumentType.string())
-                    .then(RequiredArgumentBuilder.<CommandSourceStack, String>argument("x", StringArgumentType.string())
-                        .then(RequiredArgumentBuilder.<CommandSourceStack, String>argument("y", StringArgumentType.string())
-                            .then(RequiredArgumentBuilder.<CommandSourceStack, String>argument("z", StringArgumentType.string())
-                                .executes(new RetrieveGraveStoneCommand())))));
+                .then(RequiredArgumentBuilder.<CommandSourceStack, String>argument("x", StringArgumentType.string())
+                .then(RequiredArgumentBuilder.<CommandSourceStack, String>argument("y", StringArgumentType.string())
+                .then(RequiredArgumentBuilder.<CommandSourceStack, String>argument("z", StringArgumentType.string())
+                .executes(new RetrieveGraveStoneCommand())
+                .then(RequiredArgumentBuilder.<CommandSourceStack, String>argument("confirm", StringArgumentType.string())
+                .executes(new RetrieveGraveStoneCommand()))))));
 
         commands.register(
             commandBuilder.build(),
@@ -58,6 +70,12 @@ public class RetrieveGraveStoneCommand implements Command<CommandSourceStack> {
         String y = context.getArgument("y", String.class);
         String z = context.getArgument("z", String.class);
 
+        boolean confirm = false;
+        if (context.getNodes().size() > 5) {
+            String confirmArg = context.getArgument("confirm", String.class);
+            confirm = "confirm".equalsIgnoreCase(confirmArg);
+        }
+
         // Retrieve gravestone at x, y, z
         Location gravestoneLocation = new Location(Bukkit.getWorld(world), Double.parseDouble(x), Double.parseDouble(y), Double.parseDouble(z));
 
@@ -77,6 +95,34 @@ public class RetrieveGraveStoneCommand implements Command<CommandSourceStack> {
         double distance = location.distance(gravestoneLocation);
         player.sendMessage("Distance: " + distance);
 
+        // Get cost of retrieving the gravestone every 100 blocks
+        int cost100xBlocks = 3;
+
+        // Calculate the cost of retrieving the gravestone
+        int cost = (int) Math.ceil(distance / 100) * cost100xBlocks;
+
+        // Check if the player has enough money
+        if (economy != null && economy.getBalance(player) < cost) {
+            player.sendMessage("You do not have enough money to retrieve this gravestone. Cost: " + cost);
+            return 0;
+        }
+
+        // Check if the player has confirmed the retrieval
+        if (!confirm) {
+            Component message = Component.text()
+                .append(Component.text("Retrieving the gravestone will cost you ", NamedTextColor.GRAY))
+                .append(Component.text(cost, NamedTextColor.DARK_GRAY, TextDecoration.BOLD))
+                .append(Component.text(" tokens. ", NamedTextColor.GRAY))
+                .append(Component.newline())
+                .append(Component.text("Click ", NamedTextColor.GRAY))
+                .append(Component.text("here", NamedTextColor.BLUE, TextDecoration.UNDERLINED)
+                    .clickEvent(ClickEvent.runCommand("/retrieve-gravestone " + location.getWorld().getName() + " " + location.getBlockX() + " " + location.getBlockY() + " " + location.getBlockZ() + " confirm")))
+                .append(Component.text(" to CONFIRM the retrieve of your items.", NamedTextColor.GREEN))
+                .build();
+
+            player.sendMessage(message);
+            return 0;
+        }
 
         // UUID and player related logic
         String graveOwnerString = values[0];
@@ -106,6 +152,7 @@ public class RetrieveGraveStoneCommand implements Command<CommandSourceStack> {
             Memory.deleteGravestone(gravestoneLocation);
             // Remove the gravestone block
             gravestoneLocation.getBlock().setType(Material.AIR);
+            economy.withdrawPlayer(player, cost);
         } catch (SQLException exception) {
             exception.printStackTrace();
             System.out.println("Failed to delete the database entry! " + exception.getMessage());
